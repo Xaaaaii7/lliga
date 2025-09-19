de6c71c4ef46413c5026599627b239ca39620f5b
Xaaaaii7 <103595698+Xaaaaii7@users.noreply.github.com>
2025-09-19 15:53:35 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -6,6 +6,35 @@ nav.innerHTML = `
 <a href="index.html">Noticias</a>
 <a href="clasificacion.html">Clasificación</a>
 <a href="resultados.html">Resultados</a>
+<a href="equipos.html">Equipos</a>
+<a href="jugadores.html">Jugadores</a>
+<a href="jornada.html">Jornada</a>
+`;
+})();
+
+
+// Util: carga JSON
+async function loadJSON(path){
+const res = await fetch(path);
+if(!res.ok) throw new Error('No se pudo cargar '+path);
+return res.json();
+}
+
+
+// Util: formatea fecha corta
+function fmtDate(iso){
+const d = new Date(iso);
+return d.toLocaleDateString('es-ES', { day:'2-digit', month:'short', year:'numeric' });
+}
+```js
+// Inserta nav común
+(function(){
+const nav = document.getElementById('main-nav');
+if(!nav) return;
+nav.innerHTML = `
+<a href="index.html">Noticias</a>
+<a href="clasificacion.html">Clasificación</a>
+<a href="resultados.html">Resultados</a>
 <a href="jugadores.html">Jugadores</a>
 <a href="jornada.html">Jornada</a>
 `;
