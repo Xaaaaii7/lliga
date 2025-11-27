19d5842e9a6c638b9ad73b5375aa5d0f5302a2e2
Xaaaaii7 <103595698+Xaaaaii7@users.noreply.github.com>
2025-11-27 17:11:56 +0100

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
+// ─────────────────────────────
+// CONFIGURACIÓN SUPABASE (parte superior del script)
+// ─────────────────────────────
+window.SUPABASE_CONFIG = {
+  url: "https://jdbjgrkvwawdibntngpk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkYmpncmt2d2F3ZGlibnRuZ3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMTQ4NTQsImV4cCI6MjA3OTU5MDg1NH0.t8KkjGmcCwriKfcG8pzZfCwqgddyG2jmYFxuAHH4NfA",
+  season: "2025-26"
+};
 const AppUtils = window.AppUtils || {};
 
 // Helpers base reusables
